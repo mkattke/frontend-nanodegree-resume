47bbb718e11f4ff320b545153d5835ae9876c401
mkattke <hxfreak@gmail.com>
2017-08-12 19:25:36 -0400

    Update resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,15 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var bio= {
+    "name": "Melissa Kattke",
+    "role": "SharePoint Developer",
+    "contacts":{
+        "mobile": "703-444-4323",
+        "email": "mel_kattke@gmail.com",
+        "GitHub": "mkattke",
+        "location": "DC"
+    },
+    "welcomeMsg": "blah",
+    "skills": [
+    "SP13", "SP10", "HTML", "CSS"
+    ],
+    "bioPic":"images/Mel.jpg"
+ }
